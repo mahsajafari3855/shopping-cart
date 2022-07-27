c2287f34dd2499c90509e514440a6359cc00b9b3
mahsajafari3855 <mahsa.jafari3855@gmail.com>
2022-07-27 23:28:07 +0430

    add CartContextProvider to app

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ import Store from "./components/Store";
 
 // Context
 import ProductContextProvider from "./context/ProductContextProvider";
+import CartContextProvider from "./context/CartContextProvider";
 import ProductDetails from "./components/ProductDetails";
 
 function App(props) {
   return (
     <ProductContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Store />} />
-          <Route path="/products/:id" element={<ProductDetails {...props}/>} />
+      <CartContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Store />} />
+            <Route
+              path="/products/:id"
+              element={<ProductDetails {...props} />}
+            />
 
-          <Route path="/products" element={<Store />} />
-          <Route path="*" element={<Store />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="/products" element={<Store />} />
+            <Route path="*" element={<Store />} />
+          </Routes>
+        </BrowserRouter>
+      </CartContextProvider>
     </ProductContextProvider>
   );
 }
