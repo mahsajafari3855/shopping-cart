e7a55a9faf180d26fb9b7fcc9f7f4eb6866f08ac
mahsajafari3855 <mahsa.jafari3855@gmail.com>
2022-07-28 21:42:21 +0430

    use context in product and add handler addtocart and +

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 // Functions
-import { shorten } from '../helper/functions';
+import { shorten, isInCart } from "../helper/functions";
 
-const Product = ({productData}) => {
-    
-    return (
+//context
+import { CartCaontext } from "../../context/CartContextProvider";
+
+const Product = ({ productData }) => {
+  const { state, dispatch } = useContext(CartCaontext);
+
+  return (
+    <div>
+      <img src={productData.image} alt="product" style={{ width: "200px" }} />
+      <h3>{shorten(productData.title)}</h3>
+      <p>{productData.price}</p>
+      <div>
+        <Link to={`/products/${productData.id}`}>Details</Link>
         <div>
-            <img src={productData.image} alt="product" style={{width: "200px"}} />
-            <h3>{shorten(productData.title)}</h3>
-            <p>{productData.price}</p>
-            <div>
-                <Link to={`/products/${productData.id}`}>Details</Link>
-                <div>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
+          {isInCart(state, productData.id) ? (
+            <button
+              onClick={() =>
+                dispatch({ type: "INCREASE", payload: productData })
+              }
+            >
+              +
+            </button>
+          ) : (
+            <button
+              onClick={() =>
+                dispatch({ type: "ADD_ITEM", payload: productData })
+              }
+            >
+              Add to cart
+            </button>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Product;
